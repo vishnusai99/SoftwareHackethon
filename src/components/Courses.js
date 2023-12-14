6d53d6991949aca9fe8d5e90289fd70f6e9fb273
Sai Nandan <agsnandan2004@gmail.com>
2023-12-14 22:23:31 +0530

    courses

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -2,36 +2,39 @@ import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Imag
 import { useEffect, useState } from 'react';
 
 function Courses() {
+    const [courses, setCourses] = useState([])
     return (
         <div className='container'>
-            <Card maxW='sm'>
-                    <Image
-                        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                        alt='Green double couch with wooden legs'
-                        borderTopRadius='lg'
-                    />
-                <CardBody className='w-full'>
-                    <Stack mt='6' spacing='3'>
-                        <Heading size='md'>Living room Sofa</Heading>
-                        <Text>
-                            This sofa is perfect for modern tropical spaces, baroque inspired
-                            spaces, earthy toned spaces and for people who love a chic design with a
-                            sprinkle of vintage design.
-                        </Text>
-                        <Text color='blue.600' fontSize='2xl'>
-                            $450
-                        </Text>
-                    </Stack>
-                </CardBody>
-                <Divider />
-                <CardFooter className='items-center'>
-                    <ButtonGroup spacing='2'>
-                        <Button variant='solid' colorScheme='blue'>
-                            Learn Now
-                        </Button>
-                    </ButtonGroup>
-                </CardFooter>
-            </Card>
+            {courses.map(item => {
+                return (
+                    <Card maxW='sm'>
+                        <Image
+                            src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                            alt=''
+                            borderTopRadius='md'
+                        />
+                        <CardBody className='w-full'>
+                            <Stack mt='6' spacing='3'>
+                                <Heading size='md'>{item.name}</Heading>
+                                <Text>
+                                    {item.desciption}
+                                </Text>
+                                <Text color='blue.600' fontSize='2xl'>
+                                    {item.rating}
+                                </Text>
+                            </Stack>
+                        </CardBody>
+                        <Divider />
+                        <CardFooter className='items-center'>
+                            <ButtonGroup spacing='2'>
+                                <Button variant='solid' colorScheme='blue'>
+                                    Learn Now
+                                </Button>
+                            </ButtonGroup>
+                        </CardFooter>
+                    </Card>
+                )
+            })}
         </div>
     );
 }
