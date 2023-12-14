64e8a28e0a228e2f2ea8888d189df5b85e6ecc5f
vishnusai99 <vishnusaiankam12@gmail.com>
2023-12-15 00:20:02 +0530

    changed quiz and home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,43 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import Courses from './Courses';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Courses from "./Courses";
 
 function Home() {
-    const [data, setData] = useState([])
-    const k = useLocation()
-    async function postData(selectedTags) {
-        const url = "http://127.0.0.1:5000/recommendation"
-        const data = { keywords: selectedTags, course_level: "Beginner" }
-        const response = await axios.post(url, { ...data });
-        setData(response.data.recommendations)
+  const [data, setData] = useState([]);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [goal, setGoal] = useState("");
+  const [duration, setDuration] = useState([]);
+  const [show1, setShow1] = useState(false);
+
+  const k = useLocation();
+  async function postData() {
+    const url = "http://127.0.0.1:5000/recommendation";
+    const data = { keywords: selectedTags, course_level: goal, duration };
+    const response = await axios.post(url, { ...data });
+    setData(response.data.recommendations);
+  }
+
+  useEffect(() => {
+    if (k.state) {
+      setSelectedTags(k.state.keywords);
+      setGoal(k.state.skillLevel);
+      setDuration(k.state.duration);
     }
+  }, [k?.state]);
 
-    useEffect(() => {
-        if (k.state) {
-            postData(k.state.keywords)
-        }
-    }, [k?.state])
+  useEffect(() => {
+    postData();
+  }, [selectedTags, goal, duration]);
 
-    return (
-        <div className='container mt-4 d-flex flex-row flex-wrap justify-center items-center mx-auto gap-x-4 gap-y-6'>
-            {data && data?.map((item, i) => <Courses key={i} courseData={item} />)}
-        </div>
-
-
-    );
+  return (
+    <div className="container mt-4 d-flex flex-row flex-wrap justify-center items-center mx-auto gap-x-4 gap-y-6">
+      <div>
+        <h2>Filters</h2>
+      </div>
+      {data && data?.map((item, i) => <Courses key={i} courseData={item} />)}
+    </div>
+  );
 }
 
 export default Home;
